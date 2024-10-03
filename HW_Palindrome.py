b14b79d21b4934e0346ceb178e5c9029ac8aa2a3
venkatesh-mamidipalli <mamidipalli.venkatesh@gmail.com>
2024-10-03 17:53:47 +0000

    class07

--- HW_Palindrome.py
+++
@@ -1,5 +0,0 @@
-#Palindrome 
-
-input = input("Enter a value to check if it is a palindrome: ")
-
-print(f"'{input}' is a palindrome." if input == input[::-1] else f"'{input}' is not a palindrome.")
