efe7a98c8c56f9f4b18ab021641e0b12a22ac012
venkatesh-mamidipalli <mamidipalli.venkatesh@gmail.com>
2024-10-03 16:18:28 +0000

    class07

---
+++ 02_Basics/02_String_operations/f_palindrome_check.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Purpose: Demonstration of Palindrome check
+
+    palindrome strings
+        dad
+        mom
+
+Algorithms:
+-----------
+Step 1: Take the string in run-time and store in a variable
+
+
+"""
